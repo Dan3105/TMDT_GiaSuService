8ced4c283339a03583458b24119747855271d301
DanhNguyen\Danh Nguyen <chidanh0502@gmail.com>
2024-03-30 08:08:19 +0700

    add missing files

---
+++ GiaSuService/Models/EmployeeViewModel/TutorRequestProfileViewModel.cs
@@ -0,0 +1,19 @@
+﻿using GiaSuService.Models.TutorViewModel;
+
+namespace GiaSuService.Models.EmployeeViewModel
+{
+    public class TutorRequestProfileViewModel
+    {
+        public int FormId { get; set; }
+        public string FullNameRequester { get; set; } = string.Empty;
+        public string GradeName { get; set; } = string.Empty;
+        public string SubjectName { get; set; } = string.Empty;
+        public string AddressName { get; set; } = string.Empty;
+        public DateOnly CreatedDate { get; set; }
+        public DateOnly ExpiredDate { get; set; }
+        public string CurrentStatus { get; set; } = string.Empty;
+
+        public List<TutorCardViewModel> TutorCards { get; set; } = new List<TutorCardViewModel>();
+
+    }
+}
