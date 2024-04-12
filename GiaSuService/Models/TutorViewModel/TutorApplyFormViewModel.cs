465fa3f10cc119ece8ec6108ea6048c3def75203
Kiritokun0909 <hoduchoang0909@gmail.com>
2024-04-13 02:16:08 +0700

    missing file

---
+++ GiaSuService/Models/TutorViewModel/TutorApplyFormViewModel.cs
@@ -0,0 +1,17 @@
+﻿namespace GiaSuService.Models.TutorViewModel
+{
+    public class TutorApplyFormViewModel
+    {
+        public int RequestId { get; set; }
+        public int Students { get; set; }
+
+        public string? AdditionalDetail { get; set; }
+
+        public DateTime? EnterDate { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime ExpiredDate { get; set; }
+        public string SubjectName { get; set; } = string.Empty;
+        public string GradeName { get; set; } = string.Empty;
+        public string StatusName { get; set; } = string.Empty;
+    }
+}
