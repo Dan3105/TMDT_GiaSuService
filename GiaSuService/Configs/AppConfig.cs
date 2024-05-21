51d4864375b469d2cccc3b53c3f1997a56858a69
LuongThuyVy <luongthuyvy222@gmail.com>
2024-05-21 23:54:07 +0700

    edit transaction context

--- GiaSuService/Configs/AppConfig.cs
+++ GiaSuService/Configs/AppConfig.cs
@@ -97,7 +97,7 @@ namespace GiaSuService.Configs
 
         public const int ROWS_ACCOUNT_LIST = 10;
         public static string ContextForApplyTutor(string empName, string empphone) 
-            => $"Đóng phí nhận lớp, ngay sau khí đóng hãy gửi hình ảnh qua zalo của nhân viên {empName}: {empphone}";
+            => $"Đóng phí nhận lớp, nhân viên thực hiện: {empName}, nếu có thắc mắc vui lòng liên hệ qua số điện thoại: {empphone}";
 
         public static string ContextForRefundTransaction(string tutorName, string empphone, string empName, decimal price)
         {
